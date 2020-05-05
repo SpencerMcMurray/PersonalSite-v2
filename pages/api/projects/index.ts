ed37b91362bfdaed6e64bd8743741bb5cbd21906
Spencer McMurray <spencermcmurray@yahoo.ca>
2020-05-05 14:24:16 -0400

    remove console.log

--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -58,6 +58,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     );
   }
   projects.sort((a, b) => b.stars + b.forks - (a.stars + a.forks));
-  console.log(projects);
   res.status(200).json({ projects, langs: Array.from(allLangs) });
 };
