408f6671c5ec7b606b248bf014fbd9ecb53a344d
Spencer McMurray <spencermcmurray@yahoo.ca>
2020-05-05 14:22:51 -0400

    fixed bug with sorting

--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -57,6 +57,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       })
     );
   }
-  projects.sort((a, b) => b.stars + b.forks - a.stars + a.forks);
+  projects.sort((a, b) => b.stars + b.forks - (a.stars + a.forks));
+  console.log(projects);
   res.status(200).json({ projects, langs: Array.from(allLangs) });
 };
